ea90946c789324501db1e7f2c72635c71c10b7e9
Brad Gravett <bradgravett@gmail.com>
2023-12-01 04:05:54 -0600

    Day 01 part 2 wip

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,8 @@
 import day01.day01part1
+import day01.day01part2
 
 fun main(args: Array<String>) {
-    println("day01 part1 result: ${day01part1()}")
+    if (args.isNotEmpty()) args.forEach { println("command line argument: $it") }
+    println("day 1 part 1 result: ${day01part1()}")
+    println("day 1 part 2 result: ${day01part2()}")
 }
--- src/main/kotlin/day01/day01part1.kt
+++ src/main/kotlin/day01/day01part1.kt
@@ -2,14 +2,43 @@ package day01
 
 import java.io.File
 
-fun day01part1(): Int {
-    val puzzleInput =
-        File("C:/work/AdventOfCode2023/src/main/kotlin/day01/puzzleInput.txt").bufferedReader().readLines()
+val puzzleInput = File("C:/work/AdventOfCode2023/src/main/kotlin/day01/puzzleInput.txt").bufferedReader().readLines()
+val digitWords = listOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
 
+fun day01part1(): Int {
     return puzzleInput.fold(0) { sum, line ->
         val firstDigit = line.first { it.isDigit() }.toString()
         val lastDigit = line.last { it.isDigit() }.toString()
         val concat = firstDigit + lastDigit
         sum + concat.toInt()
     }
+}
+
+fun day01part2(): Int = puzzleInput.fold(0) { sum, line ->
+    val firstDigitWord = line.findAnyOf(digitWords, 0, true)
+    val lastDigitWord = line.findLastAnyOf(digitWords, line.length, true)
+    println(
+        "first digit word: ${firstDigitWord?.second} at ${firstDigitWord?.first} | " +
+                "last digit word: ${lastDigitWord?.second} at ${lastDigitWord?.first} | " +
+                "line: $line"
+    )
+    val tensDigit = line.firstDigit()
+    val onesDigit = line.lastDigit()
+
+    sum
+}
+
+
+fun String.firstDigit(): Int {
+    val firstDigitWordAndPosition = this.findAnyOf(digitWords, 0, true)
+    val firstDigitNumber = this.find { it.isDigit() } ?: '0'
+    val firstDigitNumberPosition = this.indexOfFirst { it == firstDigitNumber }
+    val firstDigitNumberAndPosition = firstDigitNumberPosition to firstDigitNumber
+    // TODO compare positions of word and number, return value with smallest position
+return 0
+}
+
+fun String.lastDigit(): Int {
+    val lastDigitWord = this.findLastAnyOf(digitWords, this.length, true)
+    return 0
 }
