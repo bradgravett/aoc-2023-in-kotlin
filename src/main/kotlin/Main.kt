47d76bde6edcf46ef71e69cdd76fe85f035ec62f
Brad Gravett <bradgravett@gmail.com>
2023-12-01 13:25:35 -0600

    Day 01 part 2 wip 2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,14 @@
 import day01.day01part1
 import day01.day01part2
+import java.io.File
 
 fun main(args: Array<String>) {
     if (args.isNotEmpty()) args.forEach { println("command line argument: $it") }
-    println("day 1 part 1 result: ${day01part1()}")
-    println("day 1 part 2 result: ${day01part2()}")
+    val puzzleInput = File("C:/work/AdventOfCode2023/src/main/kotlin/day01/puzzleInput.txt").bufferedReader().readLines()
+    val part1TestInput = File("C:/work/AdventOfCode2023/src/main/kotlin/day01/part1TestInput.txt").bufferedReader().readLines()
+    val part2TestInput = File("C:/work/AdventOfCode2023/src/main/kotlin/day01/part2TestInput.txt").bufferedReader().readLines()
+    println("day 1 part 1 test: ${day01part1(part1TestInput)}")
+    println("day 1 part 1 solution: ${day01part1(puzzleInput)}")
+    println("day 1 part 2 test: ${day01part2(part2TestInput)}")
+    println("day 1 part 2 solution: ${day01part2(puzzleInput)}")
 }
--- src/main/kotlin/day01/day01part1.kt
+++ src/main/kotlin/day01/day01part1.kt
@@ -1,44 +1,59 @@
 package day01
 
-import java.io.File
+val digitWords = arrayListOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+val digits = arrayListOf("1", "2", "3", "4", "5", "6", "7", "8", "9")
 
-val puzzleInput = File("C:/work/AdventOfCode2023/src/main/kotlin/day01/puzzleInput.txt").bufferedReader().readLines()
-val digitWords = listOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
-
-fun day01part1(): Int {
-    return puzzleInput.fold(0) { sum, line ->
-        val firstDigit = line.first { it.isDigit() }.toString()
-        val lastDigit = line.last { it.isDigit() }.toString()
-        val concat = firstDigit + lastDigit
+fun day01part1(input: List<String>): Int {
+    return input.fold(0) { sum, line ->
+        val firstDigit = line.find { it.isDigit() } ?: '0'
+        val lastDigit = line.findLast { it.isDigit() } ?: '0'
+        val concat = firstDigit.toString() + lastDigit.toString()
         sum + concat.toInt()
     }
 }
 
-fun day01part2(): Int = puzzleInput.fold(0) { sum, line ->
-    val firstDigitWord = line.findAnyOf(digitWords, 0, true)
-    val lastDigitWord = line.findLastAnyOf(digitWords, line.length, true)
-    println(
-        "first digit word: ${firstDigitWord?.second} at ${firstDigitWord?.first} | " +
-                "last digit word: ${lastDigitWord?.second} at ${lastDigitWord?.first} | " +
-                "line: $line"
-    )
+fun day01part2(input: List<String>): Int = input.fold(0) { sum, line ->
     val tensDigit = line.firstDigit()
     val onesDigit = line.lastDigit()
 
-    sum
+    sum + (tensDigit * 10) + onesDigit
 }
 
 
 fun String.firstDigit(): Int {
-    val firstDigitWordAndPosition = this.findAnyOf(digitWords, 0, true)
-    val firstDigitNumber = this.find { it.isDigit() } ?: '0'
-    val firstDigitNumberPosition = this.indexOfFirst { it == firstDigitNumber }
-    val firstDigitNumberAndPosition = firstDigitNumberPosition to firstDigitNumber
-    // TODO compare positions of word and number, return value with smallest position
-return 0
+    val word = (findAnyOf(digitWords, 0, true) ?: (length to "0")).convertWordToDigit()
+    val number = findAnyOf(digits, 0, true) ?: (length to "0")
+
+    return when {
+        word.first < number.first -> word.second.toInt()
+        number.first < word.first -> number.second.toInt()
+        else -> 0
+    }
 }
 
 fun String.lastDigit(): Int {
-    val lastDigitWord = this.findLastAnyOf(digitWords, this.length, true)
-    return 0
+    val word = (findLastAnyOf(digitWords, length, true) ?: (0 to "0")).convertWordToDigit()
+    val number = findLastAnyOf(digits, length, true) ?: (0 to "0")
+
+    return when {
+        word.first > number.first -> word.second.toInt()
+        number.first > word.first -> number.second.toInt()
+        else -> 0
+    }
+}
+
+fun Pair<Int, String>.convertWordToDigit(): Pair<Int, String> {
+    val digit = when (second) {
+        "one" -> "1"
+        "two" -> "2"
+        "three" -> "3"
+        "four" -> "4"
+        "five" -> "5"
+        "six" -> "6"
+        "seven" -> "7"
+        "eight" -> "8"
+        "nine" -> "9"
+        else -> "0"
+    }
+    return first to digit
 }
