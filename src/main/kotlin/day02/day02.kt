f298e2bc20c5f6cede709463833155c54d8da078
Brad Gravett <bradgravett@gmail.com>
2023-12-02 13:40:56 -0600

    Day 02 part 2 complete

--- src/main/kotlin/day02/day02.kt
+++ src/main/kotlin/day02/day02.kt
@@ -2,36 +2,37 @@ package day02
 
 data class Round(val roundNumber: Int, val red: Int, val green: Int, val blue: Int)
 
-data class Game(val id: Int, val rounds: List<Round>) {
+data class Game(val input: String) {
+    val id = input.substringBefore(':').drop(5).toInt()
+    private val rounds = input.substringAfter(':').split(';').mapIndexed { index, round ->
+
+        val results = round.split(',').associate {
+            val amount = it.filter { char -> char.isDigit() }
+            val color = it.filter { char -> char.isLetter() }
+            color to amount.toInt()
+        }
+
+        Round(index + 1, results["red"] ?: 0, results["green"] ?: 0, results["blue"] ?: 0)
+    }
     private val redMax = rounds.fold(0) { max, round -> maxOf(max, round.red) }
     private val greenMax = rounds.fold(0) { max, round -> maxOf(max, round.green) }
     private val blueMax = rounds.fold(0) { max, round -> maxOf(max, round.blue) }
     val isValid = redMax <= 12 && greenMax <= 13 && blueMax <= 14
+    val power = redMax * greenMax * blueMax
 }
 
 fun part1(input: List<String>): Int {
-    val allGames: MutableList<Game> = mutableListOf()
+    val allGames = input.map { Game(it) }
 
-    input.forEach { game ->
-        val gameId = game.substringBefore(':').drop(5).toInt()
+    val sumOfAllValidGameIds = allGames.fold(0) { sum, game -> sum + if (game.isValid) game.id else 0 }
 
-        val gameRounds = game.substringAfter(':').split(';').mapIndexed { index, round ->
-
-            val results = round.split(',').associate {
-                val amount = it.filter { char -> char.isDigit() }
-                val color = it.filter { char -> char.isLetter() }
-                color to amount.toInt()
-            }
-
-            Round(index + 1, results["red"] ?: 0, results["green"] ?: 0, results["blue"] ?: 0)
-        }
-
-        allGames.add(Game(gameId, gameRounds))
-    }
-
-    return allGames.fold(0) { sum, game -> sum + if (game.isValid) game.id else 0 }
+    return sumOfAllValidGameIds
 }
 
 fun part2(input: List<String>): Int {
-    return 0
+    val allGames = input.map { Game(it) }
+
+    val sumOfAllGamePowers = allGames.fold(0) { sum, game -> sum + game.power}
+
+    return sumOfAllGamePowers
 }
